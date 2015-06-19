70babde96eac7e808db2b285ddabb271ec923bc8
Cathal McNally <mcnallycathal@yahoo.ie>
2015-06-19 17:43:42 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## The Archiver: Chrome Extension
+
+The Archiver is a tool that lets you save a webpage to the top 3 popular Archival services available today. It also lets you check if the page has already been archived.
+
+Archive.org's Wayback Machine
+Archive.is (aka: Archive.Today)
+WebCite (requires your email in the settings)
+
+Simply right click on a page and use the context menu to archive the page or check if the page has been previously saved.
+
+Save all the Internets!
+
+### Installation
+
+Download the CRX [here](https://mega.co.nz/#!qA4z1SAS!7TEZJiIEhB1JIrt2Mbrw-k0x1eutNMhOWm6P_fwibTU)
+
+To install the Chrome extension:
+
+1. Download the CRX package above.
+2. Open a new tab in Chrome and go to `chrome://extensions` in the address bar (omnibox).
+3. Drag the CRX package you downloaded from step (1) into the tab.
+
+### Usage
+
+1. Right click on a page
+2. Use the Archivers menu to Save or Check a page.
+3. Save all the Internets!
---
+++ theArchiver.js
@@ -0,0 +1,109 @@
+// Simple Chrome Extension to make it easier to Save Webpages or Check if they have been saved previously
+// uses Archive.org's Wayback Machine, Archive.is and Webcite
+// Cathal McNally
+// 19.06.2015
+// ver 1.0
+
+(function(){
+	// Get settings saved in local storage
+	var archive_org = localStorage["thearchiver_archiveorg"];
+	var archive_is = localStorage["thearchiver_archiveis"];
+	var webcite = localStorage["thearchiver_webcite"];
+	var email = localStorage["thearchiver_email"];	
+	var firstRun = localStorage["thearchiver_firstRun"];
+	
+	// Dirty but it works
+	// Check if firstRun doesnt exist, If so populate variables
+	if(firstRun == undefined){
+		// No need to check each, archive.org will do
+		if (archive_org == undefined){
+			archive_org = "true";
+			archive_is = "true";
+			webcite = "true";
+			email = "Your E-Mail";
+			
+			// turn firstRun off
+			firstRun = 0;
+			
+			// lets set the local Storage so the options Dialog is setup correctly on first run
+			localStorage["thearchiver_archiveorg"] = archive_org;
+			localStorage["thearchiver_archiveis"] = archive_is;
+			localStorage["thearchiver_webcite"] = webcite;
+			localStorage["thearchiver_email"] = email;
+		}
+	}
+	
+	// Depending on the options you have set send the currently opened page to an Archival Service
+	function saveIt(info){
+		// Save with the Wayback Machine
+		if(archive_org == "true"){
+			chrome.tabs.create({ 
+				url: "http://web.archive.org/save/" + info.pageUrl,
+			});
+		}
+		
+		// Save with Archive.is
+		if(archive_is == "true"){
+			chrome.tabs.create({ 
+				url: "https://archive.is/?run=1&url=" + info.pageUrl,
+			});
+		}
+		
+		// Save with WebCite
+		if(webcite == "true"){
+			chrome.tabs.create({ 
+				url: "http://www.webcitation.org/archive?url=" + info.pageUrl + "&email=" + email,
+			});
+		}
+	}
+
+	// Check if any of the Archival services have saved the page already
+	function checkIt(info){
+		// Check the Wayback Machine
+		if(archive_org == "true"){
+			chrome.tabs.create({ 
+				url: "http://web.archive.org/web/" + info.pageUrl,
+			});
+		}
+		
+		// Check Archive.is
+		if(archive_is == "true"){
+			chrome.tabs.create({ 
+				url: "http://archive.is/" + info.pageUrl,
+			});
+		}
+		
+		// Check WebCite
+		if(webcite == "true"){
+			chrome.tabs.create({ 
+				url: "http://www.webcitation.org/query?url=" + info.pageUrl,
+			});
+		}
+	}
+
+	// Open settings
+	function goToSettings(){
+		chrome.tabs.create({ 
+		url: "chrome-extension://efcenpmhepjlicgopcnmbnlfdbpgepmc/options/options.html",
+	  });
+	}
+	
+	// Create context menus
+	chrome.contextMenus.create({
+	  "title": "Save It",
+	  "contexts": ["page"],
+	  "onclick": saveIt}
+	);
+
+	chrome.contextMenus.create({
+	  "title": "Check It",
+	  "contexts": ["page"],
+	  "onclick": checkIt}
+	);
+
+	chrome.contextMenus.create({
+	  "title": "Settings",
+	  "contexts": ["page"],
+	  "onclick": goToSettings}
+	);
+}());
